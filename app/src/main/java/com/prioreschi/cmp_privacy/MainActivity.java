b73692f918e79a92d8cfbceef2b7edbf6f65f4a6
Armand31 <armandprioreschi@yahoo.fr>
2022-12-13 00:36:32 +0200

    Implementing Choice CMP (from Quantcast)

--- app/src/main/java/com/prioreschi/cmp_privacy/MainActivity.java
+++ app/src/main/java/com/prioreschi/cmp_privacy/MainActivity.java
@@ -3,6 +3,10 @@ package com.prioreschi.cmp_privacy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.quantcast.measurement.service.QuantcastClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        QuantcastClient.activityStart(this, BuildConfig.QUANTCAST_API_KEY, null, null);
+
+        //QuantcastClient.showQuantcastPrivacyPolicy(this); //-> Redirects to QC Privacy Policy online
+        defineListeners();
+    }
+
+    private void defineListeners() {
+
+        Button optOut = findViewById(R.id.opt_out_button);
+        optOut.setOnClickListener(view -> QuantcastClient.showAboutQuantcastScreen(MainActivity.this));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        QuantcastClient.activityStop();
     }
 }
